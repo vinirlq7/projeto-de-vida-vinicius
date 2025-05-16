5314301d246d6a08619628d595add89747b40380
vinirlq7 <145574909+vinirlq7@users.noreply.github.com>
2025-05-16 10:45:15 -0300

    16/05

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 const botoes = document.querySelectorAll(".botao");
+
+for (let i=0; i<botoes.length; i++){
+    botoes[i].onclick = function(){
+        botoes[i].classList. add("ativo");
+    }
+}
+
