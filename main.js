076a9df6e06abf1ba82eda795c513958903df2b5
vinirlq7 <145574909+vinirlq7@users.noreply.github.com>
2025-06-24 08:45:33 -0300

    24/06

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ const tempoObjetivo3 = new Date("2025-12-20T00:00:00");
 const tempoObjetivo4 = new Date("2026-07-08T00:00:00");
 
 
-contadores[0].textContent = calculaTempo(tempoObjetivo1);
+const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
 function calculaTempo(tempoObjetivo){
     let tempoAtual = new Date();
