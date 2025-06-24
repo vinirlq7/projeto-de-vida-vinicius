a404537f3f0ec1419f68b08b2be74052cb07eefc
vinirlq7 <145574909+vinirlq7@users.noreply.github.com>
2025-06-24 08:13:56 -0300

    24/06

--- main.js
+++ main.js
@@ -31,9 +31,9 @@ function calculaTempo(tempoObjetivo){
     minutos %= 60;
     horas %=60;
 
-    return (dias + "dias" + horas + "horas" + minutos + "minutos" + segundos + "segundos");
+    return (dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ");
 
-    
+
 }
 
 
