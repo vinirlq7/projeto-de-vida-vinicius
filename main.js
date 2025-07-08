db3b759b9a5f976b587a386c91740d6cd0fec254
vinirlq7 <145574909+vinirlq7@users.noreply.github.com>
2025-07-08 08:04:13 -0300

    08/07

--- main.js
+++ main.js
@@ -36,22 +36,22 @@ function calculaTempo(tempoObjetivo) {
 
     if (tempoFinal > 0) {
         return [dias, horas, minutos, segundos];
-        } else {
-        return [0,0,0,0];
+    } else {
+        return [0, 0, 0, 0];
     }
 }
-    function atualizaCronometro() {
-        for (let i = 0; i < contadores.length; i++) {
-            document.getElementById("dias" + i).textContent = calculaTempo(tempos[i])[0];
-            document.getElementById("horas" + i).textContent = calculaTempo(tempos[i])[1];
-            document.getElementById("min" + i).textContent = calculaTempo(tempos[i])[2];
-            document.getElementById("seg" + i).textContent = calculaTempo(tempos[i])[3];
-        }
+function atualizaCronometro() {
+    for (let i = 0; i < contadores.length; i++) {
+        document.getElementById("dias" + i).textContent = calculaTempo(tempos[i])[0];
+        document.getElementById("horas" + i).textContent = calculaTempo(tempos[i])[1];
+        document.getElementById("min" + i).textContent = calculaTempo(tempos[i])[2];
+        document.getElementById("seg" + i).textContent = calculaTempo(tempos[i])[3];
     }
+}
 
-    function comecaCronometro() {
-        atualizaCronometro();
-        setInterval(atualizaCronometro, 1000);
-    }
+function comecaCronometro() {
+    atualizaCronometro();
+    setInterval(atualizaCronometro, 1000);
+}
 
-    comecaCronometro();
+comecaCronometro();
